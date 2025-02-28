d41b26fe4b4425532e4ab9af5e31160fda2b67e3
geraldmas <geraldmasjesse@gmail.com>
2025-02-28 04:30:25 +0100

    Updates

---
+++ static/js/952.2542f05c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktcg_card_editor=self.webpackChunktcg_card_editor||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.2542f05c.chunk.js.map
