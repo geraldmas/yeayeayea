73827eea575c6d94a46fda53147c9268d21df77e
geraldmas <geraldmasjesse@gmail.com>
2025-06-05 21:45:00 +0200

    chore: remove unused files

--- src/utils/supabaseTest.ts
+++
@@ -1,7 +0,0 @@
-import { Card } from '../types';
-import { saveCard } from './supabaseClient';
-
-// Exécuter les tests si ce fichier est exécuté directement
-if (require.main === module) {
-  // Removed diagnostic test code
-}
